e83eaffab81d0b3fb1a5e2b5f7697f4791f9b9ef
stupachek <alona.stupak@gmail.com>
2023-04-03 19:36:41 +0300

    change c.JSON to c.AbortWithStatusJSON in some cases

--- handler/auth.go
+++ handler/auth.go
@@ -10,13 +10,12 @@ import (
 func (u *UserHandler) Register(c *gin.Context) {
 	var registerUser models.RegisterModel
 	if err := c.ShouldBindJSON(&registerUser); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"errors": err.Error()})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"errors": err.Error()})
 		return
-
 	}
 	err := u.UserService.Register(registerUser)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"errors": err.Error()})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"errors": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "Sign up was successful"})
