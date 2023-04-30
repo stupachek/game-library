cef7de52f76f195a31757fa87bd1f0092925528a
stupachek <alona.stupak@gmail.com>
2023-04-30 14:00:06 +0300

    connect database

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"game-library/api"
+	"game-library/domens/repository"
 	"log"
 	"net/http"
 )
 
 func main() {
+	_ = repository.ConnectDataBase()
 	app := api.SetupRouter()
-	err := http.ListenAndServe(":8081", app)
+	err := http.ListenAndServe(":8080", app)
 	if err != nil {
 		log.Fatal(err)
 	}
