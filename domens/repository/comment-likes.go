5a99da73b1eba8451a7a627eb547fe363777a111
Iryna Nakonechna <iryninak03@gmail.com>
2023-05-03 19:39:00 +0300

    added comment and comment likes repo

---
+++ domens/repository/comment-likes.go
@@ -0,0 +1,77 @@
+package repository
+
+import (
+	"database/sql"
+	"game-library/domens/models"
+	"github.com/google/uuid"
+	"sync"
+)
+
+type ICommentLikesRepo interface {
+	CreateCommentLike(comment models.CommentLike) error
+	Delete(id uuid.UUID) error
+}
+
+type TestCommentLikesRepo struct {
+	CommentLikes map[uuid.UUID]*models.CommentLike
+	sync.Mutex
+}
+
+func NewCommentLikesRepo() *TestCommentLikesRepo {
+	return &TestCommentLikesRepo{
+		CommentLikes: make(map[uuid.UUID]*models.CommentLike),
+	}
+}
+
+type PostgresCommentLikesRepo struct {
+	DB *sql.DB
+}
+
+func NewPostgresCommentLikesRepo(DB *sql.DB) *PostgresCommentLikesRepo {
+	return &PostgresCommentLikesRepo{
+		DB: DB,
+	}
+}
+
+func (p *PostgresCommentLikesRepo) Migrate() error {
+	query := `
+    CREATE TABLE IF NOT EXISTS commentLikes(
+        id UUID PRIMARY KEY,
+    	userId UUID,
+        commentId UUID
+    );
+    `
+	_, err := p.DB.Query(query)
+	return err
+}
+
+func (t *TestCommentLikesRepo) CreateCommentLike(commentLike models.CommentLike) error {
+	t.CommentLikes[commentLike.ID] = &commentLike
+	return nil
+}
+
+func (p *PostgresCommentLikesRepo) CreateCommentLike(commentLike models.CommentLike) error {
+	_, err := p.DB.Exec("INSERT INTO comments(id, userId, commentId) values($1, $2, $3)", commentLike.ID, commentLike.UserId, commentLike.CommentId)
+	return err
+}
+
+func (t *TestCommentLikesRepo) Delete(id uuid.UUID) error {
+	delete(t.CommentLikes, id)
+	return nil
+}
+
+func (p *PostgresCommentLikesRepo) Delete(id uuid.UUID) error {
+	res, err := p.DB.Exec("DELETE FROM commentLikes WHERE id = $1", id)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrDeleteFailed
+	}
+	return nil
+}
